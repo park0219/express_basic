56921bb998acaa862b4525e5a99f1beb71ec6e22
Park SungHwan <park0219ok@outlook.com>
2021-08-31 09:29:52 +0900

    feat: 게시글 생성 구현

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
+var qs = require("querystring");
 
 //route, routing
 app.get("/", (request, response) => {
@@ -47,6 +48,46 @@ app.get("/page/:pageID", (request, response) => {
     });
 });
 
+app.get('/create', (request, response) => {
+    fs.readdir("./data", function (error, filelist) {
+        var title = "WEB - create";
+        var list = template.list(filelist);
+        var html = template.HTML(
+            title,
+            list,
+            `
+            <form action="/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                <input type="submit">
+                </p>
+            </form>
+            `,
+            ""
+        );
+        response.send(html);
+    });
+});
+
+app.post('/create_process', (request, response) => {
+    var body = "";
+    request.on("data", function (data) {
+        body = body + data;
+    });
+    request.on("end", function () {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+            response.writeHead(302, { Location: `/?id=${title}` });
+            response.end();
+        });
+    });
+});
+
 app.listen(3000, () => console.log("Example app listening on port 3000"));
 
 /* var http = require("http");
@@ -61,80 +102,8 @@ var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    if (pathname === "/") {
-        if (queryData.id === undefined) {
-            fs.readdir("./data", function (error, filelist) {
-                var title = "Welcome";
-                var description = "Hello, Node.js";
-                var list = template.list(filelist);
-                var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
-                response.writeHead(200);
-                response.end(html);
-            });
-        } else {
-            fs.readdir("./data", function (error, filelist) {
-                var filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-                    var title = queryData.id;
-                    var sanitizedTitle = sanitizeHtml(title);
-                    var sanitizedDescription = sanitizeHtml(description, {
-                        allowedTags: ["h1"],
-                    });
-                    var list = template.list(filelist);
-                    var html = template.HTML(
-                        sanitizedTitle,
-                        list,
-                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                        ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-                    );
-                    response.writeHead(200);
-                    response.end(html);
-                });
-            });
-        }
-    } else if (pathname === "/create") {
-        fs.readdir("./data", function (error, filelist) {
-            var title = "WEB - create";
-            var list = template.list(filelist);
-            var html = template.HTML(
-                title,
-                list,
-                `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,
-                ""
-            );
-            response.writeHead(200);
-            response.end(html);
-        });
-    } else if (pathname === "/create_process") {
-        var body = "";
-        request.on("data", function (data) {
-            body = body + data;
-        });
-        request.on("end", function () {
-            var post = qs.parse(body);
-            var title = post.title;
-            var description = post.description;
-            fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-                response.writeHead(302, { Location: `/?id=${title}` });
-                response.end();
-            });
-        });
-    } else if (pathname === "/update") {
+    
+    else if (pathname === "/update") {
         fs.readdir("./data", function (error, filelist) {
             var filteredId = path.parse(queryData.id).base;
             fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
