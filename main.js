e5d6a3b51e79ac37116ccc44bc695fb5b023f02a
Park SungHwan <park0219ok@outlook.com>
2021-09-02 23:27:00 +0900

    feat: 게시글 삭제 구현

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ app.get("/page/:pageID", (request, response) => {
                 `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                 ` <a href="/create">create</a>
                 <a href="/update/${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
+                <form action="/delete_process" method="post">
                 <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
                 </form>`
@@ -128,46 +128,25 @@ app.post("/update_process", function (request, response) {
         var description = post.description;
         fs.rename(`data/${id}`, `data/${title}`, function (error) {
             fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-                response.writeHead(302, { Location: `/?id=${title}` });
-                response.end();
+                response.redirect(`/page/${title}`);
             });
         });
     });
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000"));
-
-/* var http = require("http");
-var fs = require("fs");
-var url = require("url");
-var qs = require("querystring");
-var template = require("./lib/template.js");
-var path = require("path");
-var sanitizeHtml = require("sanitize-html");
-
-var app = http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    
-    else if (pathname === "/delete_process") {
-        var body = "";
-        request.on("data", function (data) {
-            body = body + data;
+app.post("/delete_process", (request, response) => {
+    var body = "";
+    request.on("data", function (data) {
+        body = body + data;
+    });
+    request.on("end", function () {
+        var post = qs.parse(body);
+        var id = post.id;
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function (error) {
+            response.redirect("/");
         });
-        request.on("end", function () {
-            var post = qs.parse(body);
-            var id = post.id;
-            var filteredId = path.parse(id).base;
-            fs.unlink(`data/${filteredId}`, function (error) {
-                response.writeHead(302, { Location: `/` });
-                response.end();
-            });
-        });
-    } else {
-        response.writeHead(404);
-        response.end("Not found");
-    }
+    });
 });
-app.listen(3000);
- */
+
+app.listen(3000, () => console.log("Example app listening on port 3000"));
