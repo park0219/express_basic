b27f806db81420ab85629340f0219f79590c70f9
Park SungHwan <park0219ok@outlook.com>
2021-09-05 21:35:57 +0900

    feat: express 정적 파일 서비스 추가

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var qs = require("querystring");
 var bodyParser = require("body-parser");
 var compression = require("compression");
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.get("*", function (request, response, next) {
@@ -22,7 +23,7 @@ app.get("/", (request, response) => {
     var title = "Welcome";
     var description = "Hello, Node.js";
     var list = template.list(request.list);
-    var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+    var html = template.HTML(title, list, `<h2>${title}</h2>${description} <img src="/images/hello.jpg" style="width: 300px; display: block; margin:-top: 10px">`, `<a href="/create">create</a>`);
     response.send(html);
 });
 /* 위 코드와 동일함
