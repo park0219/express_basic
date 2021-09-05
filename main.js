522f9f3fc7c878a8e2fcc8882c7c88c098e9739b
Park SungHwan <park0219ok@outlook.com>
2021-09-05 22:21:27 +0900

    feat: helmet 적용

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var bodyParser = require("body-parser");
 var compression = require("compression");
 var indexRouter = require("./routes/index");
 var topicRouter = require("./routes/topic");
+var helmet = require("helmet");
 
+app.use(helmet());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
